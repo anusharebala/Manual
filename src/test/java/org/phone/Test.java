0091c9b259398de793543065960645a06639cd4b
anusharebala <anusharebala@gmail.com>
2024-09-13 13:25:57 +0530

    code addeed

--- src/test/java/org/phone/Test.java
+++ src/test/java/org/phone/Test.java
@@ -1,5 +1,14 @@
 package org.phone;
 
 public class Test {
+	public void appium() {
+		System.out.println("appium");
+	}
+	public void cucumber() {
+		System.out.println("cucumber");
 
+}
+	public void api() {
+		System.out.println("api");
+	}
 }
